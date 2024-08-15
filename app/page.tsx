e5929c042c7163225cfe94b4def1188233e068d6
Erik Hanchett <ehhance@amazon.com>
2024-08-15 14:46:26 -0700

    Updated for multi-user

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,14 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import { signOut } from 'aws-amplify/auth';
-import { fetchAuthSession } from 'aws-amplify/auth';
+import { signOut } from "aws-amplify/auth";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
-import { Authenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { getCurrentUser } from "aws-amplify/auth";
+import { Hub } from "aws-amplify/utils";
 
 Amplify.configure(outputs);
 
@@ -21,67 +22,74 @@ const client = generateClient<Schema>();
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-    function deleteTodo(id: string) {
-        client.models.Todo.delete({ id })
+  async function deleteTodo(id: string) {
+    await client.models.Todo.delete({ id });
+    await listTodos();
+  }
+
+  async function SignOut() {
+    try {
+      await signOut({ global: true });
+      setTodos([]);
+    } catch (error) {
+      console.error("Error signing out:", error);
     }
+  }
 
-    function SignOut () {
-      try {
-        // caches.keys().then((names) => {
-        //   names.forEach((name) => {
-        //       caches.delete(name);
-        //   });
-        // })
-       //location.reload();
-       //localStorage.clear();
-       //fetchAuthSession({ forceRefresh: true });
-       signOut({ global: true });
-      }
-      catch (error) {
-        console.error('Error signing out:', error);
-      }
-    };
+  async function listTodos() {
+    const { data } = await client.models.Todo.list();
+    setTodos(data);
+  }
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
+  function setHub() {
+    Hub.listen("auth", async ({ payload }) => {
+      switch (payload.event) {
+        case "signedIn":
+          await listTodos();
+          break;
+      }
     });
   }
 
   useEffect(() => {
-    listTodos();
+    getCurrentUser()
+      .then(async (user) => {
+        await listTodos();
+      })
+      .catch((err) => console.log("err", err))
+      .finally(() => setHub());
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({
+  async function createTodo() {
+    await client.models.Todo.create({
       content: window.prompt("Todo content"),
     });
+    await listTodos();
   }
 
   return (
-      <Authenticator>
-          {({ signOut, user }) => (
-           <main>
-           <h1>{user?.signInDetails?.loginId}'s todos</h1>
-           <button onClick={createTodo}>+ new</button>
-           <ul>
-               {todos.map((todo) => (
-                   <li
-                       onClick={() => deleteTodo(todo.id)}
-                       key={todo.id}>{todo.content}</li>
-               ))}
-           </ul>
-           <div>
-               🥳 App successfully hosted. Try creating a new todo.
-               <br/>
-               <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-                   Review next steps of this tutorial.
-               </a>
-           </div>
-           <button onClick={SignOut}>Sign out</button>
-       </main>
-           )
-           }
-      </Authenticator>
-  )
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>{user?.signInDetails?.loginId}'s todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo) => (
+              <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
+                {todo.content}
+              </li>
+            ))}
+          </ul>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+              Review next steps of this tutorial.
+            </a>
+          </div>
+          <button onClick={SignOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
 }
